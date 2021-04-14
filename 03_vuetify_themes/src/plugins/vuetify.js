3988bbdd2674a6a6c2a5429d221b5b130158c32e
Steven <steven.dhondt@kenze.be>
2021-04-15 00:23:59 +0200

    :lipstick: Themes

---
+++ 03_vuetify_themes/src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#41B883'
+            },
+            dark: {
+                primary: '#34495E'
+            }
+        }
+    }
+});
