ff4dcaf9afd27c01d72ed1006144792a1883f858
Steven <steven.dhondt@kenze.be>
2021-04-15 16:40:25 +0200

    :sparkles: Vue3 with TS

---
+++ 05_vacations_vue3/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Vacations title="Vacations !!!"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Vacations from '@/components/Vacations.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Vacations
+  },
+});
+</script>
+
+<style>
+  @import "design/main.css";
+</style>
---
+++ 05_vacations_vue3/src/components/Vacations.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
